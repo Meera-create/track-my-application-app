4cf95eef30c4730f4bac8cd38bf7641b561bbb82
Meera-create <126895130+Meera-create@users.noreply.github.com>
2026-04-30 19:38:55 +0100

    adding updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,24 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+
+document.getElementById("continueBtn").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const selectedOption = document.querySelector(
+        'input[name="changedName"]:checked'
+    );
+
+    if (!selectedOption) {
+        alert("Please select Yes or No");
+        return;
+    }
+
+    if (selectedOption.value === "yes") {
+        window.location.href = "/visa-date.html";
+    } else if (selectedOption.value === "no") {
+        window.location.href = "/re-do-application.html";
+    }
+});
+
 })
